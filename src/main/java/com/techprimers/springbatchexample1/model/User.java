4f2d9c1448ee396f123b4475878e38661aeda26e
navedbc <navedbc@gmail.com>
2021-12-20 22:13:01 -0500

    initial commit

---
+++ src/main/java/com/techprimers/springbatchexample1/model/User.java
@@ -0,0 +1,67 @@
+package com.techprimers.springbatchexample1.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dept='" + dept + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Id
+    private Integer id;
+    private String name;
+    private String dept;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String dept, Integer salary) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+    }
+
+    private Integer salary;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
